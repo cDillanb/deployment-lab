1d26a3a932cce870f7ba982c8b1309ad63c25d4d
Cade Bounous <bounouscade99@gmail.com>
2021-09-10 09:36:13 -0600

    more styles and organizing

--- server/index.js
+++ server/index.js
@@ -4,11 +4,15 @@ const path = require('path');
 const port = process.env.PORT || 5050;
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../index.html"));
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+
+app.get("/js", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/main.js"));
 });
 
 app.get("/css", (req, res) => {
-    res.sendFile(path.join(__dirname, '../styles.css'));
+    res.sendFile(path.join(__dirname, '../public/styles.css'));
 });
 
 app.listen(port, () => {
