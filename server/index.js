09890792b3d1ffec97bd33e949c88086434102c0
Cade Bounous <bounouscade99@gmail.com>
2021-09-10 08:34:03 -0600

    update

---
+++ server/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const path = require('path');
+const port = process.env.PORT || 5050;
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../index.html"));
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
